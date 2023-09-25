bde67cd7d117727c851f2c0348bc03c3a7fa89d4
Prajwal <prajwalbharadwaj14@gmail.com>
2023-09-25 11:03:05 +0530

    first code by prajwal

---
+++ GitHub/src/TL/FirstCode.java
@@ -0,0 +1,11 @@
+package TL;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FirstCode {
+	@Test
+	public void firstCode() {
+		Reporter.log("first code by prajwal",true);
+	}
+}
