0a55611140fef414d10c560ef210699bcb235ae6
Prajwal <prajwalbharadwaj14@gmail.com>
2023-09-25 11:18:55 +0530

    second code by prasad

---
+++ GitHub/src/T1/SecondCode.java
@@ -0,0 +1,11 @@
+package T1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SecondCode {
+	@Test
+	public void secondCode() {
+		Reporter.log("second code by prasad");
+	}
+}
